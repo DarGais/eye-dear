ef72ff572404cbb485455149c48e11d3541e4abf
Dear <siraphat.si@ku.th>
2017-06-26 17:18:17 +0900

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  root 	'static_pages#home'
-  get  	'/help',    to: 'static_pages#help'
-  get  	'/about',   to: 'static_pages#about'
-  get  	'/contact', to: 'static_pages#contact'
-  get	'/signup',	to: 'users#new'
-  post	'/signup',	to: 'users#create' 
+  root   'static_pages#home'
+  get    '/help',    to: 'static_pages#help'
+  get    '/about',   to: 'static_pages#about'
+  get    '/contact', to: 'static_pages#contact'
+  get    '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -24,5 +24,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
+    assert is_logged_in?
   end
 end
