7ad3913bb665536ab503a89673b6eef384b0d14c
Dear <siraphat.si@ku.th>
2017-06-26 13:10:06 +0900

    Finish user signup

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'coffee-rails',   '4.2.2'
 gem 'jquery-rails',   '4.3.1'
 gem 'turbolinks',     '5.0.1'
 gem 'jbuilder',       '2.6.1'
+gem 'gravtastic'
 
 group :development, :test do
   gem 'sqlite3', '1.3.13'
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get  	'/help',    to: 'static_pages#help'
   get  	'/about',   to: 'static_pages#about'
   get  	'/contact', to: 'static_pages#contact'
-  get	'/signup',	to: 'users#new' 
+  get	'/signup',	to: 'users#new'
+  post	'/signup',	to: 'users#create' 
+  resources :users
 end
