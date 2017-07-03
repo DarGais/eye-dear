9c0b3b27e025db346482f8ca9287318408530027
Dear <siraphat.si@ku.th>
2017-07-03 17:28:35 +0900

    Edit colour theme

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.       # Documentation comment
   def full_title(page_title = '')                     # Method def, optional arg
-    base_title = "Ruby on Rails Tutorial Sample App"  # Variable assignment
+    base_title = "Eye-Dear App"  # Variable assignment
     if page_title.empty?                              # Boolean test
       base_title                                      # Implicit return
     else
