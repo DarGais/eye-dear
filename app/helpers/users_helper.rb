f9de06c24440e39d0ccf1f1254ed8232f813e983
Dear <siraphat.si@ku.th>
2017-07-04 14:24:10 +0900

    Edit custom.scss

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,12 @@
 module UsersHelper
 
   # Returns the Gravatar for the given user.
-  def gravatar_for(user, options = { size: 80 })
+  def gravatar_for(user, options = {} )
+  	options = {size: 100, gravatar_class: "gravatar"}.merge(options)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
+    gravatar_class = options[:gravatar_class]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    image_tag(gravatar_url, alt: user.name, class: gravatar_class)
   end
 end
