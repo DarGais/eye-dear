86f9e12c86da6a158d8673d71fc88d83ec2ad8e1
Yoshinari Nomura <nom@nomlab.org>
2017-07-05 20:06:08 +0900

    Introduce value-class to encapsulate Field object

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -2,6 +2,30 @@ class Channel < ApplicationRecord
   has_many :sensor_values
   belongs_to :user
 
+  class Field
+    attr_reader :channel, :index, :name
+
+    def initialize(channel, index, name, enabled)
+      @channel, @index, @name, @enabled =
+        channel, index, name, enabled
+    end
+
+    def enabled?
+      @enabled
+    end
+  end
+
+  def fields
+    attribute_names.map do |col|
+      if col =~ /(field(\d)+)_name/
+        Field.new(self,
+                  $2.to_i,
+                  self[col],
+                  self["#{$1}_enable"])
+      end
+    end.compact.sort {|a,b| a.index <=> b.index}
+  end
+
   def enabled_field_numbers
     attribute_names.map do |col|
       if col =~ /field(\d)+_enable/ && self[col]
