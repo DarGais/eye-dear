7b4c8ebbf19f4d61b745dfb6be5216e29a6f3c94
Dear <siraphat.si@ku.th>
2017-07-06 11:33:02 +0900

    Change storage fog to dropbox

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -6,7 +6,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   process resize_to_limit: [400, 400]
 
   if Rails.env.production?
-    storage :fog
+    storage :dropbox
   else
     storage :file
   end
