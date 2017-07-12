e5020d7ee4aab94e89764f419c40db174760a425
Dear <siraphat.si@ku.th>
2017-07-12 10:59:24 +0900

    Change Time zone

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module SampleApp
     # -- all .rb files in that directory are automatically loaded.
     # Include the authenticity token in remote forms.
     config.action_view.embed_authenticity_token_in_remote_forms = true
-
+  config.time_zone = 'Tokyo'
+  config.active_record.default_timezone = :local
   end
 end
